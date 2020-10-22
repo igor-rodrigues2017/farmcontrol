390695bdb17ef43e8f1210e2c51de86e4890035e
Igor Rodrigues <igor.silva@softplan.com.br>
2020-10-22 00:10:37 -0300

    config for spring security and spring data

--- src/main/kotlin/br/com/igorrodrigues/farmcontrol/FarmcontrolApplication.kt
+++ src/main/kotlin/br/com/igorrodrigues/farmcontrol/FarmcontrolApplication.kt
@@ -2,12 +2,17 @@ package br.com.igorrodrigues.farmcontrol
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.web.config.EnableSpringDataWebSupport
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.config.web.servlet.invoke
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 class FarmcontrolApplication
 
 @EnableWebSecurity
@@ -15,8 +20,10 @@ class FarmControlSecurityConfiguration : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity?) {
         http {
             httpBasic {}
+            csrf { disable() }
+            sessionManagement { sessionCreationPolicy = SessionCreationPolicy.STATELESS }
             authorizeRequests {
-                authorize("/**", permitAll)
+                authorize(AntPathRequestMatcher("/signin", HttpMethod.POST.name), permitAll)
             }
         }
     }
