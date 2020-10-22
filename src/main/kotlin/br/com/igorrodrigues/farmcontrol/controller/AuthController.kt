c07583a44126b8cf40fbc1d07c76b2cfdb890083
Igor Rodrigues <igor.silva@softplan.com.br>
2020-10-22 00:11:02 -0300

    impemented controller for signin a new user

--- src/main/kotlin/br/com/igorrodrigues/farmcontrol/controller/AuthController.kt
+++ src/main/kotlin/br/com/igorrodrigues/farmcontrol/controller/AuthController.kt
@@ -1,11 +1,23 @@
 package br.com.igorrodrigues.farmcontrol.controller
 
-import org.springframework.web.bind.annotation.GetMapping
+import br.com.igorrodrigues.farmcontrol.domain.model.User
+import br.com.igorrodrigues.farmcontrol.domain.useCase.CreateUserUseCase
+import br.com.igorrodrigues.farmcontrol.domain.useCase.UserDto
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus.CREATED
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class AuthController {
 
-    @GetMapping("/auth")
-    fun teste() ="Hello World, welcome!"
+    @Autowired
+    private lateinit var createUserUseCase: CreateUserUseCase
+
+    @PostMapping("/signin")
+    fun signin(@RequestBody userDto: UserDto): ResponseEntity<User> {
+        return ResponseEntity.status(CREATED).body(createUserUseCase.create(userDto))
+    }
 }
---
+++ src/test/kotlin/br/com/igorrodrigues/farmcontrol/controller/AuthControllerTest.kt
@@ -0,0 +1,53 @@
+package br.com.igorrodrigues.farmcontrol.controller
+
+import br.com.igorrodrigues.farmcontrol.domain.model.User
+import br.com.igorrodrigues.farmcontrol.domain.useCase.CreateUserUseCase
+import br.com.igorrodrigues.farmcontrol.domain.useCase.UserDto
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.post
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension::class, MockitoExtension::class)
+internal class AuthControllerTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @MockBean
+    private lateinit var createUserUseCase: CreateUserUseCase
+
+    @Test
+    internal fun `should save a user, return user created with status 201`() {
+        val userDto = UserDto("teste@test.com", "1234")
+        `when`(createUserUseCase.create(userDto)).thenReturn(User(id = 1, email = "teste@test.com"))
+
+        mockMvc.post("/signin") {
+            contentType = APPLICATION_JSON
+            content = jacksonObjectMapper().writeValueAsString(userDto)
+            accept = APPLICATION_JSON
+        }.andExpect {
+            status { isCreated }
+            content { contentType(APPLICATION_JSON) }
+            content { json("""
+                {
+                    "id": 1,
+                    "email": teste@test.com
+                }
+            """.trimIndent()) }
+        }
+        verify(createUserUseCase).create(userDto)
+    }
+}
