6c931b24e59ad639b90e106217f7c5c4faa3d3d5
Igor Rodrigues <igor.silva@softplan.com.br>
2021-02-12 11:44:52 -0300

    refactor: simplified controller return

--- src/main/kotlin/br/com/igorrodrigues/farmcontrol/controller/farm/FarmController.kt
+++ src/main/kotlin/br/com/igorrodrigues/farmcontrol/controller/farm/FarmController.kt
@@ -2,8 +2,6 @@ package br.com.igorrodrigues.farmcontrol.controller.farm
 
 import br.com.igorrodrigues.farmcontrol.domain.usecase.farm.CreateFarmUseCase
 import br.com.igorrodrigues.farmcontrol.domain.usecase.farm.FarmDto
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -18,9 +16,7 @@ class FarmController(private val createFarmUseCase: CreateFarmUseCase) {
     @PostMapping("/add")
     fun addFarm(@RequestBody farmDto: FarmDto): ResponseEntity<String> {
         return createFarmUseCase.create(farmDto).let { ResponseEntity
-                .status(HttpStatus.CREATED)
-                .contentType(MediaType.APPLICATION_JSON)
-                .location(URI.create("/farms/$it"))
+                .created(URI.create("/farms/$it"))
                 .build() }
     }
 }
